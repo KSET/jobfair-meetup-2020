fba355e0e499e547e699da21f803ffc045e0e05e
Josip Igrec <me@allypost.net>
2020-05-15 06:36:55 +0200

    Fix axios baseUrl to always use same address for server

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.server
-  ? `http://${ process.env.HOST || "localhost" }:${ process.env.PORT || 3000 }`
+  ? "http://localhost:3000"
   : "";
 
 export default function({ $axios }, inject) {
