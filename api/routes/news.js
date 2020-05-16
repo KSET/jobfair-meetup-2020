9ee201c629854b5bab7cf7dd7befcbe4b711d160
Josip Igrec <me@allypost.net>
2020-05-16 13:19:17 +0200

    Rename news text to content

--- api/routes/news.js
+++ api/routes/news.js
@@ -14,15 +14,15 @@ const filler =
   [
     {
       title: "Ostajemo doma: Otkazan ovogodišnji Job Fair",
-      text: "S obzirom na neizvjesnost razvoja situacije s virusom COVID-19 te sukladno smjernicama Nacionalnog stožera civilne zaštite i Vlade Republike Hrvatske.",
+      description: "S obzirom na neizvjesnost razvoja situacije s virusom COVID-19 te sukladno smjernicama Nacionalnog stožera civilne zaštite i Vlade Republike Hrvatske.",
     },
     {
       title: "Budi IN na LinkedInu za 15. Job Fair!",
-      text: "Dok marljivo pripremaš svoj životopis za bazu životopisa koja je odnedavno otvorena, želimo ti približiti još jedan način kako se dodatno možeš  istaknuti na 15. Job Fairu.",
+      description: "Dok marljivo pripremaš svoj životopis za bazu životopisa koja je odnedavno otvorena, želimo ti približiti još jedan način kako se dodatno možeš  istaknuti na 15. Job Fairu.",
     },
     {
       title: "Time to shine - baza životopisa za 15. Job Fair službeno je otvorena!",
-      text: "Pripremi se - lov na karijere uskoro počinje! Ovogodišnji Job Fair, koji će se održati 13. i 14. svibnja na  Fakultetu elektrotehnike i računarstva donosi mnogo poslovnih prilika!",
+      description: "Pripremi se - lov na karijere uskoro počinje! Ovogodišnji Job Fair, koji će se održati 13. i 14. svibnja na  Fakultetu elektrotehnike i računarstva donosi mnogo poslovnih prilika!",
     },
   ]
 ;
@@ -31,7 +31,7 @@ const data =
   [
     {
       title: "Otvorene prijave za poduzeća na petnaestom Job Fairu",
-      text: "Tijekom 13. i 14. svibnja, na  Fakultetu elektrotehnike i računarstva tradicionalno će se održati Job Fair - najveći sajam poslova u Hrvatskoj - čime će FER još jednom postati žarišna točka okupljanja nekoliko tisuća studenata i preko sto poduzeća!",
+      description: "Tijekom 13. i 14. svibnja, na  Fakultetu elektrotehnike i računarstva tradicionalno će se održati Job Fair - najveći sajam poslova u Hrvatskoj - čime će FER još jednom postati žarišna točka okupljanja nekoliko tisuća studenata i preko sto poduzeća!",
     },
     ...filler,
     ...filler,
@@ -50,7 +50,7 @@ const data =
               .replace(/-+/g, "-")
               .replace(/^-+|-+$/g, "")
           }-${ i }`,
-        description: news.text.substr(0, 150),
+        content: news.description,
         image: `https://picsum.photos/id/${ i + 20 }/1920/1080`,
         imageThumbnail: `https://picsum.photos/id/${ i + 20 }/178/100`,
         date: new Date(Date.now() - (Math.random() * 3 * 24 * 60 * 60 * 1000) * (i - 1)),
