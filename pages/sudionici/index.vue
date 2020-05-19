772ed1c32c5db02bc3a491d1b8b420473ae9ba9a
Josip Igrec <me@allypost.net>
2020-05-19 10:15:35 +0200

    Add very basic participants page

--- pages/sudionici/index.vue
+++ pages/sudionici/index.vue
@@ -1,11 +1,35 @@
 <template>
-  <h1>
-    Sudionici
-  </h1>
+  <app-max-width-container>
+    <h1>
+      Sudionici
+    </h1>
+    <v-row>
+      <v-col
+        v-for="participant in participants"
+        :key="participant.id"
+        cols="6"
+        md="3"
+      >
+        {{ participant.name }}
+      </v-col>
+    </v-row>
+  </app-max-width-container>
 </template>
 
 <script>
+  import AppMaxWidthContainer from "~/components/AppMaxWidthContainer";
+
   export default {
     name: "PageSudionici",
+
+    components: {
+      AppMaxWidthContainer,
+    },
+
+    async asyncData({ store }) {
+      return {
+        participants: await store.dispatch("companies/fetchParticipants"),
+      };
+    },
   };
 </script>
