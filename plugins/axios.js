4d144db2692514a44d8bf92787b921733e8a412f
Josip Igrec <me@allypost.net>
2020-05-21 18:40:56 +0200

    Set default api axios instance to handle errors and use json as content type

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,12 +1,19 @@
 const baseUrl =
   process.server
   ? "http://localhost:3000"
-  : "";
+  : ""
+;
 
 export default function({ $axios }, inject) {
   const api = $axios.create({});
 
   api.setBaseURL(`${ baseUrl }/api`);
+  api.setHeader("Content-Type", "application/json");
+
+  api.interceptors.response.use(
+    (response) => response,
+    ({ response }) => response,
+  );
 
   inject("api", api);
 }
