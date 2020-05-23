5f0c48653db83041a15b0cef3106041c08f52b2c
Josip Igrec <me@allypost.net>
2020-05-23 12:19:31 +0200

    Add new tables to DB structure

--- db/structure.js
+++ db/structure.js
@@ -64,4 +64,53 @@ create table if not exists news
 
 create unique index if not exists news_slug_uindex
     on news (slug);
+
+create table if not exists files
+(
+    id          serial                                             not null,
+    name        varchar(255)                                       not null,
+    size        integer                                            not null,
+    hash        varchar(511)                                       not null,
+    uploader_id integer                                            not null,
+    updated_at  timestamp with time zone default CURRENT_TIMESTAMP not null,
+    created_at  timestamp with time zone default CURRENT_TIMESTAMP not null,
+    path        text                                               not null,
+    mime_type   varchar(127)                                       not null,
+    constraint files_pk
+        primary key (id)
+);
+
+create index if not exists uploads_hash_index
+    on files (hash);
+
+create index if not exists uploads_name_index
+    on files (name);
+
+create unique index if not exists files_path_uindex
+    on files (path);
+
+create table if not exists press_release
+(
+    id         serial                                             not null,
+    title      text                                               not null,
+    file_id    integer                                            not null,
+    created_at timestamp with time zone default CURRENT_TIMESTAMP not null,
+    updated_at timestamp with time zone default CURRENT_TIMESTAMP not null,
+    constraint press_release_pk
+        primary key (id),
+    constraint press_release_files_id_fk
+        foreign key (file_id) references files
+);
+
+create table if not exists press_gallery
+(
+    id          serial                                             not null,
+    title       text                                               not null,
+    description text                                               not null,
+    image_id    integer                                            not null,
+    created_at  timestamp with time zone default CURRENT_TIMESTAMP not null,
+    updated_at  timestamp with time zone default CURRENT_TIMESTAMP not null,
+    constraint press_gallery_pk
+        primary key (id)
+);
 `;
