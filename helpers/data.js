f586e5db4b32188bfdd51952f0ba158769a0e4bc
Josip Igrec <me@allypost.net>
2020-05-26 22:06:44 +0200

    Add utility to get object value by dot keys

--- helpers/data.js
+++ helpers/data.js
@@ -10,3 +10,22 @@ export const limitLength =
     (array) =>
       array.slice(0, length)
 ;
+
+export const dotGet =
+  (object, key, defaultValue = "") => {
+    const keys = key.split(".");
+
+    let k;
+    let v = object;
+    // eslint-disable-next-line no-cond-assign
+    while (v && (k = keys.shift())) {
+      v = v[k];
+    }
+
+    return (
+      v === undefined
+      ? defaultValue
+      : v
+    );
+  }
+;
