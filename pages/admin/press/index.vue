d7569a58ee57c43032457a93d7a391ce476d338d
Josip Igrec <me@allypost.net>
2020-05-28 21:28:48 +0200

    Add missing press index page

---
+++ pages/admin/press/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <app-max-width-container>
+    <v-row>
+      <v-col>
+        <h1>Press</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn
+          :to="{ name: 'PageAdminIndex' }"
+        >
+          Back
+        </v-btn>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-btn
+          :to="{ name: 'PageAdminPressReleaseList' }"
+        >
+          Press releases
+        </v-btn>
+      </v-col>
+    </v-row>
+  </app-max-width-container>
+</template>
+
+<script>
+  import AppMaxWidthContainer from "~/components/AppMaxWidthContainer";
+
+  export default {
+    name: "PageAdminPressIndex",
+
+    components: {
+      AppMaxWidthContainer,
+    },
+  };
+</script>
