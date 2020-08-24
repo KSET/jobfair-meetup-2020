c46c59fe2e8b17f486aa5a9f4476350f0cdedf9d
Josip Igrec <me@allypost.net>
2020-08-24 22:00:05 +0200

    Remove cache from resumes

--- api/routes/resumes.js
+++ api/routes/resumes.js
@@ -2,9 +2,6 @@ import {
   HttpStatus,
 } from "../helpers/http";
 import {
-  cachedFetcher,
-} from "../helpers/fetchCache";
-import {
   resumesQuery,
   resumeQuery,
 } from "../graphql/queries";
@@ -64,22 +61,14 @@ const fixResume = (resume) => {
   return fixResumes(resume);
 };
 
-const listCacheTimeoutMs = 10 * 1000;
-const fetchListCached = cachedFetcher(
-  listCacheTimeoutMs,
-  async (authHeader) => {
-    const { resumes } = await graphQlQuery(resumesQuery(), authHeader);
-
-    if (!resumes) {
-      return [];
-    }
-
-    return resumes.map(fixResume);
-  },
-);
-
 authRouter.get("/list", async ({ authHeader }) => {
-  return await fetchListCached(authHeader);
+  const { resumes } = await graphQlQuery(resumesQuery(), authHeader);
+
+  if (!resumes) {
+    return [];
+  }
+
+  return resumes.map(fixResume);
 });
 
 authRouter.get("/info/:id", async ({ authHeader, params }) => {
