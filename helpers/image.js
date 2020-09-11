fc71edf3e240031500ef7532ccee1b9b8f05b84e
Josip Igrec <me@allypost.net>
2020-09-11 20:54:15 +0200

    getSrcSet should now check whether the imageList is an object

--- helpers/image.js
+++ helpers/image.js
@@ -1,4 +1,12 @@
+import {
+ isObject,
+} from "./object";
+
 export const getSrcSet = (imageList) => {
+  if (!isObject(imageList)) {
+    return imageList;
+  }
+
   return (
     Object
       .values(imageList)
