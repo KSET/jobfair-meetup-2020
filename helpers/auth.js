72d729ffbc1d966f3a7063c9d9ae965eeecfb773
Josip <me@allypost.net>
2020-10-19 21:22:06 +0200

    Allow all moderators to be gate guardians

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,16 +1,12 @@
 import {
+  isModerator,
+} from "../api/helpers/permissions";
+import {
   dotGet,
 } from "./data";
 
 export const isGateGuardian =
   (user) =>
-    dotGet(
-      user,
-      "companies",
-      [],
-    )
-      .find(
-        ({ id }) =>
-          428 === Number(id),
-      )
+    isModerator(dotGet(user, "role")) ||
+    dotGet(user, "companies", []).find(({ id }) => 428 === Number(id))
 ;
