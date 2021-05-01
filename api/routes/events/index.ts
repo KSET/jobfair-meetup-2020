9a1788731ae44ca8c2d0166428769e3ef1cd6cb7
Josip Igrec <me@allypost.net>
2021-05-01 13:41:55 +0200

    Fix putting all event routes under moderator router

--- api/routes/events/index.ts
+++ api/routes/events/index.ts
@@ -17,12 +17,4 @@ router.get("/mine", requireCv, async ({ authUser }) => {
   return await EventReservationsService.forUser(authUser.id);
 });
 
-const moderatorRouter = AuthRouter.boundToRouter(router, {
-  role: RoleNames.MODERATOR,
-});
-
-moderatorRouter.get("/users", async ({ authHeader }) => {
-  return await EventReservationsService.listAll(authHeader);
-});
-
-export default moderatorRouter;
+export default router;
