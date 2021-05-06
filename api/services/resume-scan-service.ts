4e541c5292a954f3c4aeaf3399975e015b8aef39
Josip Igrec <me@allypost.net>
2021-05-06 04:25:26 +0200

    Add services for resume scans and favourites

---
+++ api/services/resume-scan-service.ts
@@ -0,0 +1,59 @@
+import {
+  queryResumeScansCreate,
+  queryResumeScansGetByCompanyId,
+  queryResumeScansGetByCompanyIdAndResumeId,
+  queryResumeScansUpdate,
+} from "../../db/helpers/resumeScans";
+import type {
+  ResumeScanData,
+  ResumeScan,
+} from "../../db/helpers/resumeScans";
+import {
+  Client,
+} from "../../db/methods";
+import {
+  ServiceError,
+} from "./error-service";
+import ResumeService from "./resume-service";
+import type {
+  BasicResumeInfo,
+} from "./resume-service";
+
+
+export class ResumeScanServiceError extends ServiceError {
+}
+
+export default class ResumeScanService {
+  public static async create(authHeader: string, companyId: ResumeScanData["companyId"], uid: ResumeScanData["resumeId"]): Promise<BasicResumeInfo> {
+    const resume = await ResumeService.byUid(authHeader, uid);
+
+    return await Client.transaction(async (client) => {
+      const key: ResumeScanData = {
+        companyId,
+        resumeId: resume.id,
+      };
+
+      const hasScan = await client.queryOne<ResumeScan>(queryResumeScansGetByCompanyIdAndResumeId(key));
+
+      if (hasScan) {
+        await client.queryOne(queryResumeScansUpdate(key));
+      } else {
+        await client.queryOne(queryResumeScansCreate(key));
+      }
+
+      return resume;
+    });
+  }
+
+  public static async listFor(companyId: ResumeScanData["companyId"]): Promise<ResumeScan["resume_id"][]> {
+    const rawResumeIds = await Client.queryOnce<ResumeScan>(queryResumeScansGetByCompanyId({
+      companyId,
+    }));
+
+    if (!rawResumeIds) {
+      return [];
+    }
+
+    return rawResumeIds.map(({ resume_id: id }) => id);
+  }
+}
--- db/helpers/resumeFavourites.ts
+++ db/helpers/resumeFavourites.ts
@@ -1,13 +1,34 @@
+import type {
+  SnakeCasedPropertiesDeep,
+} from "type-fest";
+import type {
+  Company,
+  Resume,
+} from "../../api/graphql/types";
+import type {
+  Query,
+} from "../methods";
 import {
   generateInsertQuery,
 } from "../query";
+
+/* eslint-disable camelcase */
+
+export interface ResumeFavouriteData {
+  companyId: Company["id"];
+  resumeId: Resume["id"];
+}
+
+export type ResumeFavourite = SnakeCasedPropertiesDeep<ResumeFavouriteData>;
+
+/* eslint-enable camelcase */
 
 export const queryResumeFavouritesGetByCompanyId =
   (
     {
       companyId,
-    },
-  ) => ({
+    }: Pick<ResumeFavouriteData, "companyId">,
+  ): Query => ({
     text: `
       select
         *
@@ -27,8 +48,8 @@ export const queryResumeFavouritesGetByCompanyIdAndResumeId =
     {
       companyId,
       resumeId,
-    },
-  ) => ({
+    }: ResumeFavouriteData,
+  ): Query => ({
     text: `
       select
         *
@@ -50,8 +71,8 @@ export const queryResumeFavouritesDeleteByCompanyIdAndResumeId =
     {
       companyId,
       resumeId,
-    },
-  ) => ({
+    }: ResumeFavouriteData,
+  ): Query => ({
     text: `
       delete from
         resume_favourites
@@ -71,8 +92,8 @@ export const queryResumeFavouritesCreate =
     {
       companyId,
       resumeId,
-    },
-  ) => generateInsertQuery({
+    }: ResumeFavouriteData,
+  ): Query => generateInsertQuery({
     table: "resume_favourites",
     data: {
       companyId,
