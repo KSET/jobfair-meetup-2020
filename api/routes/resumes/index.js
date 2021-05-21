2f244a4dba7d01243dd021dc202a12926f637f33
Josip Igrec <me@allypost.net>
2021-05-21 15:42:32 +0200

    Remove resume pdf proxy

--- api/routes/resumes/index.js
+++ api/routes/resumes/index.js
@@ -1,10 +1,4 @@
 import {
-  withoutKeys,
-} from "../../../helpers/object";
-import {
-  get,
-} from "../../helpers/axios";
-import {
   HttpStatus,
 } from "../../helpers/http";
 import {
@@ -30,33 +24,6 @@ authRouter.get("/for-user/:uid", async ({ authHeader, params }) => {
   }
 
   return resume;
-});
-
-authRouter.getRaw("/info/:id.pdf", async ({ authHeader, params, headers: reqHeaders }, res) => {
-  const { id } = params;
-  const resume = await ResumeService.byId(authHeader, id);
-
-  try {
-    if (!resume) {
-      throw new ApiError("Resume not found", HttpStatus.Error.Client.NotFound);
-    }
-
-    const { resumeFileData } = resume;
-
-    const headers = withoutKeys([ "cookie", "host", "referer", "connection" ], reqHeaders);
-    const response = await get(resumeFileData, {
-      responseType: "stream",
-      headers,
-    });
-
-    res.header(response.headers);
-
-    response.pipe(res);
-    response.on("end", () => resume.end());
-  } catch (e) {
-    res.status(HttpStatus.Error.Client.NotFound);
-    return res.end();
-  }
 });
 
 authRouter.get("/info/:id", async ({ authHeader, params }) => {
